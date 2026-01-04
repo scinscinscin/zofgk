f1ad89665cd129da91b749f8166223f82596e5d5
scinscinscin <scinorandex1@gmail.com>
2026-01-05 07:23:21 +0800

    funny chovy moment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  redirects: async () => ([{ source: "/", destination: "https://www.youtube.com/watch?v=paaKAlx8plk", permanent: true }])
 };
 
 export default nextConfig;
